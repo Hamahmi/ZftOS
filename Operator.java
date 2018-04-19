3c3ba9a334cc6f067ff362103f2c6fedb3be7be3
MostafaShehab <35817197+MostafaShehab@users.noreply.github.com>
2018-04-19 22:36:24 +0200

    Update Operator.java

--- Operator.java
+++ Operator.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class Operator {
@@ -10,32 +12,79 @@ public class Operator {
 	public static Wheel wheel;
 	public static int totalPlayers;
 	public static CyclicBarrier gate;
+	public static Queue<Player> queue = new LinkedList<>();
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new FileReader("input-1.txt"));
 		int maxWaitingTime = Integer.parseInt(br.readLine());
 		totalPlayers = Integer.parseInt(br.readLine());
 		wheel = new Wheel(5, maxWaitingTime);
-		gate = new CyclicBarrier((totalPlayers + 2));
+		gate = new CyclicBarrier((totalPlayers + 3));
+		wheel.start();
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			if (!(line.equals(""))) {
+				String[] l = line.split(",");
+				int id = Integer.parseInt(l[0]);
+				int wt = Integer.parseInt(l[1]);
+				Player p = new Player(id, wt);
+				p.start();
+			}
+		}
+		br.close();
+		Thread t = new Thread(){
+			public void run(){
+				try {
+					gate.await();
+					while(true){
+						//System.out.println("here");
+						synchronized(queue){
+							if(queue.isEmpty()){
+								//Thread.sleep(1);
+							}else{
+								if(wheel.nOnboard < wheel.capacity){
+									Player p = queue.poll();
+									System.out.println("Player " + p.ID + " on board, capacity: " + (wheel.nOnboard + 1));
+									wheel.load_players(p);
+								}
+								if(wheel.nOnboard == wheel.capacity){
+									wheel.run_ride();
+									wheel.end_ride();
+								}
+							}
+						}	
+					}
+				} catch (InterruptedException | BrokenBarrierException e) {
+					e.printStackTrace();
+				}
+			}	
+		};
+		t.start();
+		try {
+			gate.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static class Wheel extends Thread{
 		int capacity;
 		int nOnboard;
-		ArrayList<Player> onbaord;
+		ArrayList<Player> onboard;
 		int maxWaitingTime;
 		public Wheel(int capacity, int maxWaitingTime){
 			this.capacity = capacity;
 			this.nOnboard = 0;
-			this.onbaord = new ArrayList<>();
+			this.onboard = new ArrayList<>();
 			this.maxWaitingTime = maxWaitingTime;
 		}
 		@Override
 		public void run(){
 			try{
+				gate.await();
 				System.out.println("wheel start sleep");
 				Wheel.sleep(maxWaitingTime);
-			}catch(InterruptedException e){}
+			}catch(InterruptedException | BrokenBarrierException e){}
 			finally{
 				System.out.println("wheel end sleep");
 			}
@@ -44,19 +93,29 @@ public class Operator {
 		public synchronized void load_players(Player player){
 			nOnboard++;
 			totalPlayers--;
-			onbaord.add(player);
+			onboard.add(player);
 		}
 		public synchronized void run_ride(){
 			System.out.println("Wheel is ready, Let's go for a ride");
 			System.out.println("Threads in this ride are:");
-			for(Player p : onbaord){
+			for(Player p : onboard){
 				System.out.print(p.ID+", ");
 			}
 			System.out.println();
 		}
 		public synchronized void end_ride(){
 			nOnboard = 0;
-			onbaord = new ArrayList<>();
+			onboard = new ArrayList<>();
+			if(totalPlayers==0){
+				System.exit(0);
+			}
+			try{
+				System.out.println("wheel start sleep");
+				Wheel.sleep(maxWaitingTime);
+			}catch(InterruptedException e){}
+			finally{
+				System.out.println("wheel end sleep");
+			}
 		}
 	}
 	public static class Player extends Thread{
@@ -65,6 +124,19 @@ public class Operator {
 		public Player(int ID, int waitingTime){
 			this.ID = ID;
 			this.waitingTime = waitingTime;
+		}
+		
+		@Override
+		public void run(){
+			try{
+				gate.await();
+				Player.sleep(waitingTime);
+				System.out.println("player wakes up: " + this.ID);
+				System.out.println("passing player: " + this.ID + " to the operator");
+				synchronized(queue){
+					queue.add(this);
+				}
+			}catch(InterruptedException | BrokenBarrierException e){}
 		}
 	}
 	
