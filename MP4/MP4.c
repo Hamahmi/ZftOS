e18adab1a1225de0cbfe293180abadb4d51d3882
MostafaShehab <mostafa.shehab4858@gmail.com>
2018-05-05 13:06:11 +0200

    Added basic memory structure

--- MP4/MP4.c
+++ MP4/MP4.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+struct Page {
+  int PageID;
+  bool Referenced;
+  bool Modified;
+};
+
+#define MAX 5
+
+struct Page array[MAX];
+int pageCount = 0;
+
+bool isEmpty() {
+  return pageCount == 0;
+}
+
+bool isFull() {
+  return pageCount == MAX;
+}
+
+void insert (struct Page page){
+  array[pageCount] = page;
+  pageCount++;
+}
+
+struct Page remove (int index){
+  int i;
+  for (i = index; i < MAX-1; i++){
+    array[i] = array[i+1];
+  }
+  array[MAX-1] = NULL;
+  pageCount--;
+}
+
+bool checkExist (int pageID){
+  int i;
+  for (i = 0; i < pageCount){
+    if(array[i].PageID == pageID){
+      return true;
+    }
+  }
+  return false;
+}
 
 int main(){
-  
+
 }
